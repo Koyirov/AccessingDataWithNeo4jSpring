b066eaabe48e0212a53e363788e9f6d8ddc037b5
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-14 07:18:00 +0200

    initial commit

---
+++ src/test/java/com/example/accessingdatawithneo4jspring/AccessingDataWithNeo4jSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatawithneo4jspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataWithNeo4jSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
